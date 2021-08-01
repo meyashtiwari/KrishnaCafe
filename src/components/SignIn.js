649e85a6d08faf6e4ddb086844aa4bd47c979794
Yash Tiwari <me.yashtiwari@gmail.com>
2021-08-02 01:47:48 +0530

    All login and signup related functionality has been added

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,47 +1,54 @@
-import React, { useState } from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import { Link } from 'react-router-dom';
-import Grid from '@material-ui/core/Grid';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import { Typography } from '@material-ui/core';
+import React, { useState } from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import { Link, useHistory } from "react-router-dom";
+import Grid from "@material-ui/core/Grid";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import { Typography } from "@material-ui/core";
 
-import fire from '../config/fire';
+import fire from "../config/fire";
 
 const useStyles = makeStyles((theme) => ({
 	paper: {
 		marginTop: theme.spacing(8),
-		display: 'flex',
-		flexDirection: 'column',
-		alignItems: 'center',
+		display: "flex",
+		flexDirection: "column",
+		alignItems: "center",
 	},
 	avatar: {
 		margin: theme.spacing(1),
 		backgroundColor: theme.palette.secondary.main,
 	},
 	form: {
-		width: '100%', // Fix IE 11 issue.
+		width: "100%", // Fix IE 11 issue.
 		marginTop: theme.spacing(1),
 	},
 	submit: {
 		margin: theme.spacing(3, 0, 2),
-		backgroundColor: '#e31837',
+		backgroundColor: "#e31837",
 	},
 }));
 
 export default function SignIn() {
 	const classes = useStyles();
-	const [email, setEmail] = useState('');
-	const [password, setPassword] = useState('');
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
 
-	const loginUser = () => {
-		fire.auth().signInWithEmailAndPassword(email, password);
+	let history = useHistory();
+
+	const loginUser = async () => {
+		try {
+			await fire.auth().signInWithEmailAndPassword(email, password);
+			history.push("/menu");
+		} catch (error) {
+			console.error(error);
+		}
 	};
 
 	return (
@@ -54,10 +61,7 @@ export default function SignIn() {
 				<Typography component="h1" variant="h5">
 					Login
 				</Typography>
-				<form
-					className={classes.form}
-					onSubmit={(e) => e.preventDefault() && false}
-				>
+				<form className={classes.form} onSubmit={(e) => e.preventDefault() && false}>
 					<TextField
 						variant="outlined"
 						margin="normal"
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,49 +1,49 @@
-import React, { useState } from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import { Link, useHistory } from 'react-router-dom';
-import Grid from '@material-ui/core/Grid';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+import React, { useState } from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import { Link, useHistory } from "react-router-dom";
+import Grid from "@material-ui/core/Grid";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
 
-import fire from '../config/fire';
-import { useMutation } from 'urql';
-import { registerNewCustomer } from '../queries/registerNewCustomer';
+import fire from "../config/fire";
+import { useMutation } from "urql";
+import { registerNewCustomer } from "../queries/registerNewCustomer";
 
 const useStyles = makeStyles((theme) => ({
 	paper: {
 		marginTop: theme.spacing(8),
-		display: 'flex',
-		flexDirection: 'column',
-		alignItems: 'center',
+		display: "flex",
+		flexDirection: "column",
+		alignItems: "center",
 	},
 	avatar: {
 		margin: theme.spacing(1),
 		backgroundColor: theme.palette.secondary.main,
 	},
 	form: {
-		width: '100%', // Fix IE 11 issue.
+		width: "100%", // Fix IE 11 issue.
 		marginTop: theme.spacing(3),
 	},
 	submit: {
 		margin: theme.spacing(3, 0, 2),
-		backgroundColor: '#e31837',
+		backgroundColor: "#e31837",
 	},
 }));
 
 export default function SignUp() {
 	const classes = useStyles();
 
-	const [firstName, setFirstName] = useState('');
-	const [lastName, setLastName] = useState('');
-	const [email, setEmail] = useState('');
-	const [password, setPassword] = useState('');
+	const [firstName, setFirstName] = useState("");
+	const [lastName, setLastName] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
 
 	const [result, registerCustomer] = useMutation(registerNewCustomer);
 	let history = useHistory();
@@ -64,10 +64,10 @@ export default function SignUp() {
 				if (result.error) {
 					console.log(result.error);
 				} else {
-					console.log('Registered Successfully');
+					console.log("Registered Successfully");
 				}
 			});
-			history.push('/menu');
+			history.push("/menu");
 		} catch (error) {
 			console.error(error);
 		}
@@ -83,10 +83,7 @@ export default function SignUp() {
 				<Typography component="h1" variant="h5">
 					Sign up
 				</Typography>
-				<form
-					className={classes.form}
-					onSubmit={(e) => e.preventDefault() && false}
-				>
+				<form className={classes.form} onSubmit={(e) => e.preventDefault() && false}>
 					<Grid container spacing={2}>
 						<Grid item xs={12} sm={6}>
 							<TextField
